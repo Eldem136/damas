dc1cb4121e4b553e9f31429ad45e1edde476d430
diego <diego@diego-PC.unizar.es>
2016-02-17 16:35:23 +0100

    se ha cambiado el main

--- Damas/src/damas/Damas.java
+++ Damas/src/damas/Damas.java
@@ -15,8 +15,9 @@ public class Damas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Esto es una prueba");
+        
+        System.out.println("y esto es la segunda");
     }
     
 }
