c1f5d45c7a37736162f59231107ed922bfbf8c4a
diego <diego@diego-PC.unizar.es>
2016-03-02 17:29:43 +0100

    Se crean los esqueletos de Reglas, ReglasDamas y Movimiento

---
+++ damas_v1/src/reglas/ReglasDamas.java
@@ -0,0 +1,34 @@
+
+package reglas;
+
+import utilidades.Movimiento;
+import damas.Tablero;
+
+/**
+ *
+ * @author diego
+ */
+public class ReglasDamas implements Reglas {
+    
+    public ReglasDamas (){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean mover(Movimiento mov, Tablero tabl) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean movimientoValido(Movimiento mov, Tablero tabl) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean hayGanador() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+}
---
+++ damas_v1/src/utilidades/Movimiento.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilidades;
+
+/**
+ *
+ * @author diego
+ */
+public class Movimiento {
+    private int filaInicial;
+    private int colInicial;
+    private int filaFinal;
+    private int colFinal;
+    
+    /**
+     * 
+     * @param fila1 la fila de la posicion actual de la ficha
+     * @param col1 la columna de la posicion actual de la ficha
+     * @param fila2 la fila de la posicion final de la ficha
+     * @param col2 la columna de la posicion final de la ficha
+     */
+    public Movimiento ( int fila1, int col1, int fila2, int col2 ) {
+
+    }
+
+    /**
+     * @return la fila actual
+     */
+    public int getFilaInicial() {
+        return filaInicial;
+    }
+
+    /**
+     * @return la columna actual
+     */
+    public int getColInicial() {
+        return colInicial;
+    }
+
+    /**
+     * @return la fila final
+     */
+    public int getFilaFinal() {
+        return filaFinal;
+    }
+
+    /**
+     * @return la columna final
+     */
+    public int getColFinal() {
+        return colFinal;
+    }
+    
+    
+}
