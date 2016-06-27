20b0cb6879fa11df02820abcac55b9e2bbb135b6
diego <diego@diego-PC.telefonica.net>
2016-06-27 12:19:28 +0200

    añadida clase con mensajes para la comunicacion

---
+++ damas_v3/src/damas/MensajesConexion.java
@@ -0,0 +1,30 @@
+/**
+ * MensajesConexion.java
+ * @author Ezequiel Barbudo     (zeko3991@gmail.com)
+ * @author Diego Malo           (d.malo136@gmail.com)
+ */
+package damas;
+
+public class MensajesConexion {
+    /*
+    La clase MensajesConexion proporciona los strings que serviran para 
+    realizar una comunicacion sin errores entre servidor y cliente
+    */
+    
+    public final static String ACTUALIZAR_LISTA_USUARIOS = "actualizar_lista";
+    public final static String GANADOR = "ganador";
+    public final static String PERDEDOR = "perdedor";
+    public final static String ACEPTAR_RETO = "aceptar_reto";
+    public final static String CANCELAR_RETO = "cancelar_reto";
+    public final static String RETO_ACEPTADO = "reto_aceptado";
+    public final static String RETO_RECHAZADO = "reto_rechazado";
+    public final static String SALIR = "salir";
+    public final static String FIN_TURNO = "fin_turno";
+    public final static String EMPIEZA_TURNO = "empieza_turno";
+    public final static String MOVIMIENTO = "movimiento";
+    public final static String RENDICION = "rendicion";
+    public final static String CERRAR = "cerrar";
+    public final static String RETAR = "retar";
+    public final static String OK = "OK";
+    
+}
